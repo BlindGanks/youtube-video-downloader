3ea7e88dc91c1e8dd98a9d3df07bd21be5d10c15
blindganks <abdelhaktebak@gmail.com>
2022-10-04 17:06:46 +0100

    data downloaded progress

--- downloadvid.js
+++ downloadvid.js
@@ -1,10 +1,12 @@
 const ytdl = require("ytdl-core");
 const fs = require("fs");
-const homeDir = require("os").homedir(); // See: https://www.npmjs.com/package/os
+const homeDir = require("os").homedir();
 const desktopDir = `${homeDir}/Desktop`;
 const downloadButton = document.getElementById("download-button");
 const progressBar = document.getElementById("progress-bar");
-const progressText = document.getElementById("progress-text");
+const progressContainer = document.getElementById("progress-container");
+const percentageElement = document.getElementById("progress-percentage");
+const fileSizeElement = document.getElementById("file-size-progress");
 
 //  const url = document.getElementById("input").value;
 const selectedFormat = document.getElementById("format").value;
@@ -12,8 +14,8 @@ const selectedFormat = document.getElementById("format").value;
 const downloadVideo = async () => {
   const url = "https://www.youtube.com/watch?v=bx3--22D4E4";
   if (!url || !selectedFormat) return;
-  progressText.innerHTML = "0 %";
-  progressBar.style.opacity = "100%";
+  percentageElement.innerHTML = "0 %";
+  progressContainer.style.opacity = "100%";
 
   try {
     const { videoDetails } = await ytdl.getBasicInfo(url);
@@ -26,7 +28,12 @@ const downloadVideo = async () => {
     );
 
     stream.on("progress", (_, downloaded, total) => {
-      showProgress({ percentage: Math.round((downloaded * 100) / total) });
+      showProgress({
+        percentage: Math.round((downloaded * 100) / total),
+        total: humanFileSize(total).size,
+        downloaded: humanFileSize(downloaded).size,
+        sizeUnit: humanFileSize(total).unit,
+      });
 
       if (downloaded === total) {
         resetProgressElements();
@@ -44,13 +51,40 @@ const downloadVideo = async () => {
 };
 
 const resetProgressElements = () => {
-  progressText.innerHTML = "";
-  progressBar.style.opacity = "0%";
+  percentageElement.innerHTML = "";
+  progressContainer.style.opacity = "0%";
 };
 
-const showProgress = ({ percentage = 0 }) => {
+const showProgress = ({ percentage = 0, downloaded, total, sizeUnit }) => {
   progressBar.firstElementChild.style.width = `${percentage}%`;
-  progressText.innerHTML = `${percentage} %`;
+  percentageElement.innerHTML = `${percentage} %`;
+  fileSizeElement.innerHTML = `${downloaded} / ${total} ${sizeUnit}`;
+};
+
+const humanFileSize = (bytes, si = true, dp = 1) => {
+  const thresh = si ? 1000 : 1024;
+
+  if (Math.abs(bytes) < thresh) {
+    return bytes + " B";
+  }
+
+  const units = si
+    ? ["kB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"]
+    : ["KiB", "MiB", "GiB", "TiB", "PiB", "EiB", "ZiB", "YiB"];
+  let u = -1;
+  const r = 10 ** dp;
+
+  do {
+    bytes /= thresh;
+    ++u;
+  } while (
+    Math.round(Math.abs(bytes) * r) / r >= thresh &&
+    u < units.length - 1
+  );
+  return {
+    unit: units[u],
+    size: bytes.toFixed(dp),
+  };
 };
 
 downloadButton.addEventListener("click", downloadVideo);
